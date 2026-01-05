8ad27f48aa0810a150a2fbd6d401a2e8ad33bb8c
den4ik <denzel8028@hotmail.com>
2026-01-05 16:04:07 +0300

    remade bio ( again >:) )

---
+++ resources/errorPageStrings.js
@@ -0,0 +1,14 @@
+﻿//Split out for localization.
+var L_GOBACK_TEXT = "Перейдите на предыдущую страницу.";
+var L_REFRESH_TEXT = "Обновите страницу.";
+var L_MOREINFO_TEXT = "Подробнее";
+var L_OFFLINE_USERS_TEXT = "Для автономного использования";
+var L_RELOAD_TEXT = "Повторите ввод адреса.";
+var L_HIDE_HOTKEYS_TEXT = "Быстрые приемы работы со вкладками";
+var L_SHOW_HOTKEYS_TEXT = "Показать другие способы быстрого переключения вкладок";
+
+//used by invalidcert.js
+var L_CertUnknownCA_TEXT = "Сертификат безопасности этого веб-узла не был выпущен доверенным центром сертификации.";
+var L_CertExpired_TEXT = "Срок действия сертификата безопасности этого веб-узла истек или еще не начинался.";
+var L_CertCNMismatch_TEXT = "Сертификат безопасности этого веб-узла был выпущен для веб-узла с другим адресом.";
+var L_CertRevoked_TEXT = "Сертификат этой организации отозван.";
