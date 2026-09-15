1d4ce89a60cd493acb50d759e21bcd9f22b51ed0
mossad <razvedka@mossad.il>
2026-09-15 23:40:33 +0300

    stupid hack to fix 'Back' action

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const links = document.querySelectorAll('a');
+function bindTransitions() {
+    const links = document.querySelectorAll('a');
+    const transitionScreen = document.getElementById("transition-screen");
 
-links.forEach(link => {
-    link.onclick = function(event)
-    {
-        document.getElementById("transition-screen").classList.add("startTransition");
-        event.preventDefault(); 
-        
-        const transitionScreen = document.getElementById("transition-screen");
-        const targetUrl = this.getAttribute('href');
-        if (transitionScreen) {
+    if (!transitionScreen) return;
+
+    links.forEach(link => {
+        link.onclick = function(event) {
+            const targetUrl = this.getAttribute('href');
+            
+            event.preventDefault(); 
+            
             transitionScreen.classList.add("startTransition");
             
             setTimeout(() => {
                 window.location.href = targetUrl;
             }, 1000);
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', bindTransitions);
+
+window.addEventListener('pageshow', (event) => {
+    if (event.persisted) {
+        const transitionScreen = document.getElementById("transition-screen");
+        if (transitionScreen) {
+            transitionScreen.classList.remove("startTransition");
         }
     }
 });
